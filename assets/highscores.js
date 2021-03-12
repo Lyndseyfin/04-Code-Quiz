9247319eef0f27290bca4625070d69a1dbca3e56
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-03-12 13:27:35 -0700

    hs

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,3 +1,9 @@
 //get items from local storage and show them in highscores page
 //loop through array of scores
-var scores = []
+var highScoresList = document.querySelector('#highScoresList')
+var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+highScoresList.innerHTML =
+highScores.map(score => {
+    return <li class="high-score">${score.name} - $(score.score)</li>
+}).join('')
